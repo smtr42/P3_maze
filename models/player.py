05eddfde96d094f2df057663bd2539ec73e18a8f
smtr42 <simteiva@gmail.com>
2019-04-19 20:50:25 +0200

    Created the Player class
    Added the move method and comments for further dev

--- models/player.py
+++ models/player.py
@@ -1,1 +1,23 @@
+from position import Position
+from level import Level
 
+class Player:
+
+    def __init__(self):
+        self.position = Level
+        self.total_item = []
+
+    def move(self, direction):
+        # modify current coordinate according
+        # to the direction and what position return
+        print(getattr(Position, "up"))
+
+    # def pick_up_item(self):
+    #     # if item position == player position
+    #     # then add item to item_count
+    #     # delete
+    #     pass
+
+
+plm = Player()
+plm.move("up")

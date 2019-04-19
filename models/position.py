85b1f89ce7ba10311f2f0a3f6dc6d36793ea2a1a
smtr42 <simteiva@gmail.com>
2019-04-19 20:49:17 +0200

    added theclass position
    added the 4 methods to calculate the new positions

--- models/position.py
+++ models/position.py
@@ -1,1 +1,35 @@
+"""This module will contain everything about
+the position : calculations and checks, either
+for the player or the items"""
 
+
+# from .level import Level
+
+class Position:
+    """This class will calculate every position
+    according to the direction provided"""
+
+    def __init__(self, x, y):
+        self.position = (x, y)
+
+    def __repr__(self):
+        return str(self.position)
+
+    def up(self):
+        x, y = self.position
+        return Position(x + 1, y)
+
+    def down(self):
+        x, y = self.position
+        return Position(x - 1, y)
+
+    def right(self):
+        x, y = self.position
+        return Position(x, y + 1)
+
+    def left(self):
+        x, y = self.position
+        return Position(x, y - 1)
+
+
+pos = Position(1, 1)
